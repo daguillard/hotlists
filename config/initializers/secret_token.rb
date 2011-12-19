da5acf1d91271e883f4ba3ceb8b1979f6276382e
David Aguillard <d_aguillard@hotmail.com>
2011-12-19 17:32:47 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hotlists::Application.config.secret_token = '117af20955ca71569d25f0638b364b70cc63edf5db0f9fe9b306ab5d3453bfe878dde18c8d59be4332d9cbbf15ca4abee9bd7bb8b4d839f7b1627fd9fe013d0b'
